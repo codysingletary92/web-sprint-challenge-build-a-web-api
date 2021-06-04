baaef6e6b9964d5abd04ce3643c906c3301dd427
samuel singletary <codysingletary92@gmail.com>
2021-06-04 17:19:52 -0400

    mvp finished

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,7 @@
 // Write your "projects" router here!
 const express = require('express');
 const Projects = require('./projects-model')
+const { logger, validateUserId, validateUser, validatePost } = require('../middleware/middleware')
 
 
 
@@ -17,19 +18,35 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     Projects.get(req.params.id)
-    .then( project => res.status(200).json(project))
+    .then( project => {
+        console.log(project)
+        if (project === null){
+           
+           return res.status(404).send('proejct not found')
+        } 
+        return res.status(200).json(project)
+    })
     .catch( err => res.status(500).json(err))
 })
 
 router.post('/', (req, res) => {
+    console.log(req.body.name === undefined)
+    console.log(req.body.description === undefined)
+
+    if (req.body.description === undefined){
+        console.log('here?')
+        res.status(400)
+    }
+
     Projects.insert(req.body)
-    .then(proejct => res.status(201).send(json(req.body)))
-    .catch( err => res.status(400).json(err))
+    .then(proejct => res.status(201).json(req.body)
+    )
+    .catch( err => res.status(500).json(err))
 })
 
 router.put('/:id', (req, res) => {
     Projects.update(req.params.id, req.body)
-    .then(project => res.status(202).send(json(project, 'updated')))
+    .then(project => res.status(202).json(project, 'updated'))
     .catch(err => res.status(400).json(err))
 })
 
