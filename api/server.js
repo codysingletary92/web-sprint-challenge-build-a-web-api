1dce0a2170b3c07be4ea3f0cd9c341421d281295
samuel singletary <codysingletary92@gmail.com>
2021-06-04 13:31:36 -0400

    server running basic route

--- api/server.js
+++ api/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json())
+
+const actionsRoutes = require('./actions/actions-router');
+const projectsRoutes = require('./projects/projects-router');
+
+// server.use('/actions', actionsRoutes)
+// server.use('/projects', projectsRoutes)
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Server working</h1>`)
+})
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
